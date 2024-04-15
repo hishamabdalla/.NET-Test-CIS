14302d9237c14581c08a8d9661c7d1ea29c21e6e
Hisham Abdalla <hishvmabdalla@gmail.com>
2024-04-15 17:37:59 +0200

    Second Task

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle(4,3);
+            Rectangle rectangle2 = new Rectangle(5,8);
+
+            rectangle1.Calculateperimeter();
+            rectangle1.CalculateArea();
+            rectangle2.Calculateperimeter();
+            rectangle2.CalculateArea();
+            rectangle1.Display(nameof(rectangle1));
+            rectangle2.Display(nameof(rectangle2));
+
+        }
+    }
+}
---
+++ Task2/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Rectangle
+    {
+        public int Length {  get; set; }
+        public int Width { get; set; }
+
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public int CalculateArea() => Length * Width;
+        public int Calculateperimeter() => 2*( Length * Width);
+
+        public void Display(string message)
+        {
+            Console.WriteLine($"======{ message.ToUpper()}======");
+            Console.WriteLine($"The Area Of Rectangle = {CalculateArea()}\n" +
+                $" The Perimeter Of Rectangle = {Calculateperimeter()}\n");
+            
+        }
+
+
+
+    }
+}
