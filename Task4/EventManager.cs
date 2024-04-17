ee869409cb91c100901a5dd92179b0decade32da
Hisham Abdalla <hishvmabdalla@gmail.com>
2024-04-17 04:44:30 +0200

    Fourth Task

---
+++ Task4/EventManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public delegate void EventHandler();
+    class EventManager
+    {
+        public event EventHandler CustomEvent;
+
+        public void TriggerEvent()
+        {
+           
+                CustomEvent?.Invoke();
+            
+        }
+        public void AddEventHandler(EventHandler handler)
+        {
+            CustomEvent += handler;
+        }
+
+        public void RemoveEventHandler(EventHandler handler)
+        {
+            CustomEvent -= handler;
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventManager eventManager = new EventManager();
+            eventManager.AddEventHandler(Event1);
+            eventManager.AddEventHandler(Event2);
+
+            Console.WriteLine("Event triggered:");
+            eventManager.TriggerEvent();
+
+            eventManager.RemoveEventHandler(Event1);
+
+            Console.WriteLine("\n after removing  handler:");
+            eventManager.TriggerEvent();
+
+        }
+
+        private static void Event1()
+        {
+            Console.WriteLine("Event1 invoked");
+        }
+
+        private static void Event2()
+        {
+            Console.WriteLine("Event2 invoked");
+        }
+    }
+}
