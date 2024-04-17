85fc73e867509c1675db18f7841985e4dea8a123
Hisham Abdalla <hishvmabdalla@gmail.com>
2024-04-17 05:26:40 +0200

    Fifth Task

---
+++ Task5/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Calculator
+    {
+        public static int addition(int x, int y) => x + y;
+        public static int subtraction(int x, int y) => x - y;
+        public static int multiplication(int x, int y) => x * y;
+        public static int division(int x, int y) => x / y;
+
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        public delegate int MathOperation(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers: ");
+            string[] input = Console.ReadLine().Split(" ");
+
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+
+            MathOperation Add = Calculator.addition;
+            MathOperation subtract = Calculator.subtraction;
+            MathOperation multiply = Calculator.multiplication;
+            MathOperation divide = Calculator.division;
+
+            Console.WriteLine($"Addition: {x} + {y} = {Add(x, y)}");
+
+            Console.WriteLine($"Subtraction: {x} - {y} = {subtract(x, y)}");
+
+            Console.WriteLine($"Multiplication: {x} * {y} = {multiply(x, y)}");
+
+            Console.WriteLine($"Division: {x} / {y} = {divide(x, y)}");
+        }
+       
+
+    }
+}
